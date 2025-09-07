e2e0c9d99e17d1f72ad7c5a2cd7453bfb07c911a
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2025-09-07 08:24:59 +0530

    Step 2nd Year

--- WEEK3/LABWORK/q2.java
+++ WEEK3/LABWORK/q2.java
@@ -1,3 +1,18 @@
+class Book{
+    private String BookId;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    private static int totalBooks = 0;
+    private static int availableBooks = 0;
+    private static int bookCounter = 1;
+
+    public Book(String title , String author){
+        this.title = title;
+    }
+}
+
 public class q2 {
     public static void main(String[] args){ 
         
